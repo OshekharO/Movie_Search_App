c5fd11067443e171f84908ad1b2cdea0cd54a8b1
Saksham Shekher <95137948+OshekharO@users.noreply.github.com>
2022-02-16 09:42:07 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -262,27 +262,6 @@ function showVideos() {
     })
 }
 
-const leftArrow = document.getElementById('left-arrow')
-const rightArrow = document.getElementById('right-arrow')
-
-leftArrow.addEventListener('click', () => {
-    if (activeSlide > 0) {
-        activeSlide--;
-    } else {
-        activeSlide = totalVideos - 1;
-    }
-    showVideos()
-})
-
-rightArrow.addEventListener('click', () => {
-    if (activeSlide < (totalVideos - 1)) {
-        activeSlide++;
-    } else {
-        activeSlide = 0;
-    }
-    showVideos()
-})
-
 function getColor(vote) {
     if (vote >= 8) {
         return 'green'
