8dda0f5902b086e4367bea13a03499687ce4d45f
Saksham Shekher <95137948+OshekharO@users.noreply.github.com>
2022-02-16 11:18:57 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -207,15 +207,15 @@ function showMovies(data) {
         movieEl.classList.add('movie');
         movieEl.innerHTML = `
             <img src="${poster_path? IMG_URL+poster_path: "http://via.placeholder.com/1080x1580" }" alt="${title}">
-
             <div class="movie-info">
                 <h3>${title}</h3>
                 <span class="${getColor(vote_average)}">${vote_average}</span>
             </div>
-
             <div class="overview">
-                <h3 style="text-align:center;">WATCH</h3>
-                <button class="know-more" id="${id}">WATCH NOW</button
+                <h3> <button class="know-more" id="${id}">Know More</button> </h3>
+       <br/>
+         ${overview}
+                 
             </div>
         
         `
