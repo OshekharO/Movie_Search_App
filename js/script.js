b8fb698acd08de3b69f98b6978698fe566e85e7a
Saksham Shekher <95137948+OshekharO@users.noreply.github.com>
2022-05-10 15:44:38 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -170,7 +170,7 @@ function openNav(movie) {
                     let { name, key, site } = video
                     if (site == 'YouTube') {
                         embed.push(`
-                            <iframe width="100%" height="100%" src="https://www.2embed.ru/embed/tmdb/movie?id=${id}" title="${name}" 
+                            <iframe width="100%" height="100%" src="https://2embed.org/embed/movie?tmdb=${id}" title="${name}" 
                             class="embed hide" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; 
                             gyroscope; picture-in-picture" allowfullscreen></iframe>
                         `)
